316b97ab7cd1172aa20e71164feaf9133f03058f
Imran <imranlm677@gmail.com>
2021-03-04 16:45:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const users = [];
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded());
+
+app.get("/", (req, res, next) => {
+  res.render("index", { pageTitle: "Add User" });
+});
+
+app.get("/users", (req, res, next) => {
+  res.render("Users", { pageTitle: "User", users: users });
+});
+
+app.post("/add-user", (req, res, next) => {
+  users.push({ name: req.body.username });
+  res.redirect("/users");
+});
+
+app.listen(3000, () => {
+  console.log("Listening at 3000");
+});
